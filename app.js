c8528c53bff88e474d02acd0e3e1cbe7c937a22e
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2015-05-02 16:03:16 +1000

    create net connection

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('nodemon running...')
+var net = require('net')
+// Keep track of the chat clients
+var clients = [];
+
+net.createServer(function (socket) {
+
+
+
+}).listen(3000,"0.0.0.0");
+
+// Put a friendly message on the terminal of the server.
+console.log("Chat server running at port 3000\n");
