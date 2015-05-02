79887f0dc5c7007c88def7122c00f4add1c8072a
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2015-05-02 16:11:17 +1000

    socket basic behaviour

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ var clients = [];
 
 net.createServer(function (socket) {
 
+  // Identify this client
+  socket.name = socket.remoteAddress + ":" + socket.remotePort
 
+  // Put this new client in the list
+  clients.push(socket);
+
+  // Send a nice welcome message and announce
+  socket.write("Welcome " + socket.name + "\n");
+  broadcast(socket.name + " joined the chat\n", socket);
+
+  getAll(socket)
+
+  // Handle incoming messages from clients.
+  socket.on('data', function (data) {
+    broadcast(socket.name + ' >> ' + data + '\n', socket)
+  });
+
+  // Remove the client from the list when it leaves
+  socket.on('end', function () {
+    clients.splice(clients.indexOf(socket), 1);
+    broadcast(socket.name + " left the chat!!!\n");
+  });
 
 }).listen(3000,"0.0.0.0");
 
