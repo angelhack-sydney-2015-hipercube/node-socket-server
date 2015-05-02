d9841c7c8f3f0197517f136d47b80a1b72b0afd4
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2015-05-02 16:46:16 +1000

    configuring cluster point connection

--- cp-api.js
+++ cp-api.js
@@ -1,16 +1,19 @@
 var cps = require('cps-api')
 
-var conn = new cps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'posts', 'manuel@gizmomen.com', 'tiburcio34', 'document', '//document/id', {account: 100034});
+var conn = new cps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'docs', 'luigibertaco@gmail.com', 'unsecurePass123', 'document', '//document/id', {account: 100091});
 
 // GET ALL
 exports.all = function (callback) {
- var search = new cps.SearchRequest('*',0,99999)
- conn.sendRequest(search, function(err, resp){
-   if (err){
-     console.error(err)
-     return callback(err)
-   }else{
-     return callback(null, resp.results.document)
-   }
- })
+  var search = new cps.SearchRequest('*',0,99999)
+  conn.sendRequest(search, function(err, resp){
+    if (err){
+      console.error(err)
+      return callback(err)
+    }else{
+      if(resp.results)
+        return callback(null, resp.results.document)
+      else
+        return callback(null, {})
+    }
+  })
 };
