03f7306b999a885ef56930a8c75c52302baf1dd5
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2015-05-02 16:51:24 +1000

    add CRUD by json on socket

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ net.createServer(function (socket) {
 
   // Handle incoming messages from clients.
   socket.on('data', function (data) {
-    broadcast(socket.name + ' >> ' + data + '\n', socket)
+    var req = JSON.parse(data)
+    if(req.request == 'list')   getAll(socket)
+    if(req.request == 'get')    getObj(socket, req.id)
+    if(req.request == 'add')    addObj(socket, req.obj)
+    if(req.request == 'edit')   editObj(socket, req.obj)
+    if(req.request == 'delete') deleteObj(socket, req.obj)
   });
 
   // Remove the client from the list when it leaves
@@ -45,6 +50,39 @@ net.createServer(function (socket) {
       if(err) return console.error('err')
       console.log('client ' + client.name + ' >> getAll')
       client.write(JSON.stringify(data)+"\n")
+    })
+  }
+
+  function getObj(client, id){
+    cp.obj(id, function(err, data){
+      if(err) return console.error('err')
+      console.log('client ' + client.name + ' >> getObj')
+      client.write(JSON.stringify(data))
+    })
+  }
+
+  function addObj(client, obj){
+    cp.add(obj, function(err, data){
+      if(err) return console.error('err')
+      console.log('client ' + client.name + ' >> addObj')
+      //client.write(JSON.stringify(data))
+      broadcast(JSON.stringify(data),null)
+    })
+  }
+
+  function editObj(client, obj){
+    cp.edit(obj, function(err, data){
+      if(err) return console.error('err')
+      console.log('client ' + client.name + ' >> editObj')
+      broadcast(JSON.stringify(data),null)
+    })
+  }
+
+  function deleteObj(client, obj){
+    cp.delete(obj, function(err, data){
+      if(err) return console.error('err')
+      console.log('client ' + client.name + ' >> deleteObj')
+      broadcast(JSON.stringify(data),null)
     })
   }
 
--- cp-api.js
+++ cp-api.js
@@ -17,3 +17,59 @@ exports.all = function (callback) {
     }
   })
 };
+//GET ONE
+exports.obj = function (id, callback) {
+  var search = new cps.SearchRequest(cps.Term(id, "id"))
+  conn.sendRequest(search, function(err, resp){
+    if (err){
+      console.error(err)
+      return callback(err)
+    }else{
+      if(resp.results)
+        return callback(null, resp.results.document[0])
+      else
+        return callback(null, {})
+    }
+  })
+};
+
+// ADD
+exports.add = function (obj, callback) {
+  obj.id = crypto.randomBytes(20).toString('hex');
+  var insert_req = new cps.InsertRequest(obj);
+  conn.sendRequest(insert_req, function (err, list_resp) {
+    if (err){
+      console.error(err)
+      return callback(err)
+    }else{
+      return callback(null, obj)
+    }
+  });
+};
+
+// EDIT
+exports.edit = function (obj, callback) {
+  var  upd = new cps.UpdateRequest(obj)
+  conn.sendRequest(upd, function(err, resp){
+    if (err){
+      console.error(err)
+      return callback(err)
+    }else{
+      return callback(null, obj)
+    }
+  })
+};
+
+// DELETE
+exports.delete = function (obj, callback) {
+  var  del = new cps.DeleteRequest(obj)
+  conn.sendRequest(del, function(err, resp){
+    if (err){
+      console.error(err)
+      return callback(err)
+    }else{
+      obj.deleted = true;
+      return callback(null, obj)
+    }
+  })
+};
